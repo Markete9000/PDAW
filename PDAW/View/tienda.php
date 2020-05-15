a6a312019837d196005da5b161414b2c424cc9b1
Marco <Markete9000@gmail.com>
2020-05-15 22:47:05 +0200

    15/05/2020 Segundo commit

--- PDAW/View/tienda.php
+++ PDAW/View/tienda.php
@@ -19,31 +19,69 @@
 
     <div class="contenedor">
 
+        <div class="contenedor-menu">
+
+            <!-- Este botón posteriormente quizá lo elimine -->
+            <!-- <a href="#" class="btn-menu">Productos <i class="icono fa fa-bars"></i></a> -->
+
+            <ul class="menu">
+
+                <li><a href="#">Componentes <i class="icono derecha fa fa-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Placas Base</a></li>
+                        <li><a href="#">Procesadores</a></li>
+                        <li><a href="#">Discos Duros</a></li>
+                        <li><a href="#">Tarjetas Gráficas</a></li>
+                        <li><a href="#">Memoria RAM</a></li>
+                        <li><a href="#">Torres</a></li>
+                        <li><a href="#">Ventilación</a></li>
+                        <li><a href="#">Fuente de alimentación</a></li>
+                        <li><a href="#">Discos duros</a></li>
+                    </ul>
+                </li>
+
+                <li><a href="#">Periféricos <i class="icono derecha fa fa-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Monitores</a></li>
+                        <li><a href="#">Teclados</a></li>
+                        <li><a href="#">Ratones</a></li>
+                    </ul>
+                </li>
+
+                <li><a href="#">Smartphones <i class="icono derecha fa fa-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Samsung</a></li>
+                        <li><a href="#">Iphone</a></li>
+                        <li><a href="#">Xiaomi</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+
         <div class="productos">
 
-        <?php
-        
-        foreach ($data['productos'] as $producto) {
-            
-            echo '<div class="caja-imagen">';
-                echo '<img class="imagen" src="'.$producto->getImagen().'" alt="">';
-                echo '<h4 class="titulo">'.$producto->getNombre().'</h4>';
-                echo '<h3 class="precio">'.$producto->getPrecio().'€</h3>';
-                echo '<div class="botones">';
-                    echo '<form action="../Controller/ficha-producto.php" method="post">';
-                        echo '<input type="hidden" name="codigo" value="'.$producto->getCodigo().'">';
-                        echo '<input class="info" type="submit" value="Más Info">';
-                    echo '</form>';
-                    echo '<form action="../Controller/añadir_carrito.php" method="post">';
-                        echo '<input type="hidden" name="codigo" value="'.$producto->getCodigo().'">';
-                        echo '<input class="añadir" type="submit" value="Comprar">';
-                    echo '</form>';
+            <?php
+            foreach ($data['productos'] as $producto) {
+                
+                echo '<div class="caja-imagen">';
+                    echo '<img class="imagen" src="'.$producto->getImagen().'" alt="">';
+                    echo '<h4 class="titulo">'.$producto->getNombre().'</h4>';
+                    echo '<h3 class="precio">'.$producto->getPrecio().'€</h3>';
+                    echo '<div class="botones">';
+                        echo '<form action="../Controller/ficha-producto.php" method="post">';
+                            echo '<input type="hidden" name="codigo" value="'.$producto->getCodigo().'">';
+                            echo '<input class="info" type="submit" value="Más Info">';
+                        echo '</form>';
+                        echo '<form action="../Controller/añadir_carrito.php" method="post">';
+                            echo '<input type="hidden" name="codigo" value="'.$producto->getCodigo().'">';
+                            echo '<input class="añadir" type="submit" value="Comprar">';
+                        echo '</form>';
+                    echo '</div>';
                 echo '</div>';
-            echo '</div>';
 
-        }
-        
-        ?>
+            }
+            ?>
 
         </div>
 
